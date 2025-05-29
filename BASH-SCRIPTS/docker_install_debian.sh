f52dcd37fbedf536977ec902fd7d6258d9a10c4e
Jdawg520 <110195685+Jdawg520@users.noreply.github.com>
2025-05-28 23:14:44 -0700

    Update docker_install_debian.sh

--- BASH-SCRIPTS/docker_install_debian.sh
+++ BASH-SCRIPTS/docker_install_debian.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+cat << "EOF"
+
+ ____             _               
+|  _ \  ___   ___| | _____ _ __   
+| | | |/ _ \ / __| |/ / _ \ '__|  
+| |_| | (_) | (__|   <  __/ |     
+|____/ \___/ \___|_|\_\___|_| _ _ 
+      |_ _|_ __  ___| |_ __ _| | |
+       | || '_ \/ __| __/ _` | | |
+       | || | | \__ \ || (_| | | |
+      |___|_| |_|___/\__\__,_|_|_|
+
+EOF
+
+cat << EOF
+This script will install Docker and Docker Compose
+on this system.
+
+Copyright 2025-$(date +'%Y'), Jonathan Syposs.
+
+===================================================
+
+EOF
+
+
 # Update Repositories
 
 sudo apt update && sudo apt upgrade -y
